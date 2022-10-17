9c05d7ce444d200a0acaec59bda6831ba69edd32
Thabish <kadertabish2@gmail.com>
2022-10-17 11:39:26 +0400

    75%ui complete

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,10 +1,26 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { fruits } from "../data";
+import { dairy, fruits, vegetable } from "../data";
 import { FoodData } from "../interface/interface";
 
 export const Services = () => {
-	const [food, setFood] = useState<FoodData[]>(fruits);
+	const [data, setData] = useState<FoodData[]>(vegetable);
+	const allData: FoodData[] = [...dairy, ...fruits, ...vegetable];
+
+	const handleVegetables = (): void => {
+		setData(vegetable);
+	};
+
+	const handleFruits = (): void => {
+		setData(fruits);
+	};
+
+	const handleDairy = (): void => {
+		setData(dairy);
+	};
+	const handleAll = (): void => {
+		setData(allData);
+	};
 
 	return (
 		<section className="flex h-full w-full max-w-[1660px] flex-col items-center justify-center p-10">
@@ -15,23 +31,35 @@ export const Services = () => {
 			<div className=" flex flex-col items-center justify-center ">
 				<h2 className="text-xl font-bold ">Filter type</h2>
 				<div className=" my-5 grid min-w-[500px] grid-cols-4 flex-wrap justify-between">
-					<button className="  w-[100px] rounded-xl bg-green-500 text-black duration-500 hover:scale-110 hover:bg-white hover:text-green-500">
+					<button
+						onClick={handleAll}
+						className="  w-[100px] rounded-xl bg-green-500 text-black duration-500 hover:scale-110 hover:bg-white hover:text-green-500"
+					>
 						All
 					</button>
-					<button className="  w-[100px] rounded-xl bg-green-500 text-black duration-500 hover:scale-110 hover:bg-white hover:text-green-500">
+					<button
+						onClick={handleVegetables}
+						className="  w-[100px] rounded-xl bg-green-500 text-black duration-500 hover:scale-110 hover:bg-white hover:text-green-500"
+					>
 						Vegetables
 					</button>
-					<button className=" w-[100px] rounded-xl bg-green-500 text-black duration-500 hover:scale-110 hover:bg-white hover:text-green-500">
+					<button
+						onClick={handleFruits}
+						className=" w-[100px] rounded-xl bg-green-500 text-black duration-500 hover:scale-110 hover:bg-white hover:text-green-500"
+					>
 						Fruits
 					</button>
-					<button className=" w-[100px] rounded-xl bg-green-500 text-black duration-500 hover:scale-110 hover:bg-white hover:text-green-500">
+					<button
+						onClick={handleDairy}
+						className=" w-[100px] rounded-xl bg-green-500 text-black duration-500 hover:scale-110 hover:bg-white hover:text-green-500"
+					>
 						Dairy
 					</button>
 				</div>
 			</div>
 			{/* Items */}
 			<div className="ls:grid-cols3 grid grid-cols-1 gap-6 pt-4 md:grid-cols-2 xl:grid-cols-4">
-				{food.map((data, id) => (
+				{data.map((data, id) => (
 					<div
 						key={id}
 						className="h-[460px] rounded-lg border-b-2 border-green-500 duration-500 hover:scale-110"
